0355e9b28b4a6b96ebb41b4d5013736aae40020a
steffeApp <steffe71@gmail.com>
2026-05-27 23:16:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -315,3 +315,18 @@ function closeHelp() {
 window.addEventListener("load", () => {
   showHelp();
 });
+let lastTouchEnd = 0;
+
+document.addEventListener(
+  "touchend",
+  function (event) {
+    const now = Date.now();
+
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+
+    lastTouchEnd = now;
+  },
+  { passive: false }
+);
